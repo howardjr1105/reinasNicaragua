8f66c372ab98c46b5dc8bcb749d3ce6a26d7f16d
Kevin Garcia <kevin.garcia@ahinko.com>
2024-08-09 15:33:15 -0600

    Generic response

---
+++ ReinasApiPrueba/Models/GenricResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ReinasApiPrueba.Models
+{
+    public class GenricResponse
+    {
+        public bool success { get; set; }
+        public string Message { get; set; }
+        public Object Data { get; set; }
+    }
+}
